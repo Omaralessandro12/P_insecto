5291b4a296a893dfd9164163442a93dd3b8e925e
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-11-21 17:45:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ import helper
 
 # Setting page layout
 st.set_page_config(
-     page_title="Deteccion de Plagas en la agricultura Mexicana",
-     layout="wide",
-     initial_sidebar_state="expanded"
+page_title="Deteccion de Plagas en la agricultura Mexicana",
+layout="wide",
+initial_sidebar_state="expanded"
 )
 
 # Encabezado 
