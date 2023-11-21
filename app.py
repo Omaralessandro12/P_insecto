0c83da0b755a81b5bf804fa9774020a05b560c11
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-11-21 17:53:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -51,44 +51,5 @@ source_img = None
 if source_radio == settings.IMAGE:
     source_img = st.sidebar.file_uploader(
         "Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+ 
 
-    col1, col2 = st.columns(2)
-
-    with col1:
-        try:
-            if source_img:
-                uploaded_image = PIL.Image.open(source_img)
-                st.image(source_img, caption="Imagen Cargada",
-                         use_column_width=True)
-        except Exception as ex:
-            st.error("Se produjo un error al abrir la imagen.")
-            st.error(ex)
-
-    with col2:        
-            if st.sidebar.button('Detectar Objeto'):
-                res = model.predict(uploaded_image,
-                                    conf=confidence
-                                    )
-                boxes = res[0].boxes
-                res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Detected Image',
-                         use_column_width=True)
-                try:
-                    with st.expander("Resultados de la detección"):
-                        for box in boxes:
-                            st.write(box.data)
-                except Exception as ex:
-                    # st.write(ex)
-                    st.write("No image is uploaded yet!")
-
-#elif source_radio == settings.VIDEO:
-#    helper.play_stored_video(confidence, model)
-
-elif source_radio == settings.WEBCAM:
-    helper.play_webcam(confidence, model)
-
-#elif source_radio == settings.YOUTUBE:
-#   helper.play_youtube_video(confidence, model)
-
-else:
-    st.error("Please select a valid source type!")
